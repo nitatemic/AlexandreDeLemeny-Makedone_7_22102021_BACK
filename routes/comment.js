3db6645a49893d48eeae1a17d36981d6c2fb4908
nitatemic <alexandre@nitatemic.dev>
2021-12-24 16:01:05 +0100

    Added a route to get some comments

--- routes/comment.js
+++ routes/comment.js
@@ -7,5 +7,6 @@ const commentController = require("../controllers/comment.js");
 const router = express.Router();
 router.get("/", authMiddleware.verifyToken, dbConnectMiddleware.getCommentFromDB, commentController.getAllComment);
 router.post("/", authMiddleware.verifyToken, commentController.addComment);
-
+router.get("/:from/:to", authMiddleware.verifyToken, dbConnectMiddleware.getCommentsFromTo, commentController.getAllComment); //Route get pour envoyer les comment du numéro X à Y
 module.exports = router;
+

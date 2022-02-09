70f367b6d29191a33a2192992db2cc0f431b044b
nitatemic <alexandre@nitatemic.dev>
2022-02-09 04:52:07 +0100

    Create account routes #8

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
 const commentRoutes = require('./routes/comment');
 const isAliveRoutes = require('./routes/isAlive');
+const accountRoutes = require ('./routes/account');
 
 const app = express();
 app.use(helmet());
@@ -23,6 +24,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
+app.use('/api/account', accountRoutes);
 app.use('/', isAliveRoutes);
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,5 @@ const dbConnectMiddleware = require('../middlewares/dbConnect');
 const router = express.Router();
 router.post('/signup', authMiddleware.AllFieldsCompleted, authMiddleware.checkMail, userCtrl.createUser);
 router.post('/login', authMiddleware.AllFieldsCompletedForLogin, authMiddleware.checkMail, dbConnectMiddleware.getCredentials, userCtrl.login);
+
 module.exports = router;

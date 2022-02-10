ee6522121241cca39f4e8aad103a6e4aafd8f21d
nitatemic <alexandre@nitatemic.dev>
2022-02-10 09:30:06 +0100

    SQL query to delete a user account #12

--- controllers/account.js
+++ controllers/account.js
@@ -1,6 +1,5 @@
 // Fonction qui appel le middleware qui fait l'appel des informations de profil à la BDD
 exports.getUserInfos = (req, res) => {
-  console.log(res.locals.user);
   res.status(200).json({
     user: res.locals.user,
   });
--- middlewares/dbConnect.js
+++ middlewares/dbConnect.js
@@ -307,3 +307,22 @@ exports.getUserInfo = (req, res, next) => {
     );
   });
 };
+
+// Fonction de suppression de compte utilisateur
+exports.deleteUser = (req, res, next) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err; // not connected!
+    // Use the connection
+    connection.query(
+      `DELETE FROM users WHERE PersonID = ${res.locals.PersonID};`,
+      (error, results) => {
+        // When done with the connection, release it.
+        connection.release();
+        // Handle error after the release.
+        if (error) throw error;
+        res.locals.SQLResponse = results;
+        next();
+      },
+    );
+  });
+};
